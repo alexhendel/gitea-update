0c3442d638ff7b9a4c3732945a35c090d9fedc63
Alexander Hendel <alexander.hendel@gmail.com>
2024-04-27 08:24:36 +0200

    Fix version comparison and oputput for info flag

--- internal/version/version.go
+++ internal/version/version.go
@@ -20,41 +20,31 @@ func CheckInstalledVersion(path, binaryName string) string {
 	output, err := cmd.Output()
 	if err != nil {
 		logger.LogWarn(fmt.Sprintf("Error executing %s --version: %s\n", binaryName, err))
-		return ""
+		return "n/a"
 	}
 
 	scanner := bufio.NewScanner(strings.NewReader(string(output)))
 	if scanner.Scan() {
 		line := scanner.Text()
-		return extractVersion(line, binaryName)
+		return extractVersion(line)
 	}
 
 	if err := scanner.Err(); err != nil {
 		logger.LogError(fmt.Sprintf("Error reading output from %s --version: %s\n", binaryName, err))
 	}
 
-	return ""
+	return "n/a"
 }
 
-func extractVersion(line, binaryName string) string {
-	switch binaryName {
-	case "gitea":
-		// Expected: "Gitea version 1.21.10 built with..."
-		parts := strings.Split(line, " ")
-		for i, part := range parts {
-			if part == "version" && i+1 < len(parts) {
-				return strings.TrimSpace(parts[i+1])
-			}
-		}
-	case "act_runner":
-		// Expected: "act_runner version v0.2.9"
-		parts := strings.Split(line, " ")
-		for i, part := range parts {
-			if part == "version" && i+1 < len(parts) {
-				return strings.TrimPrefix(strings.TrimSpace(parts[i+1]), "v")
-			}
+func extractVersion(line string) string {
+	// Expected: "Gitea version 1.21.10 built with..." or "act_runner version v0.2.9"
+	parts := strings.Split(line, " ")
+	for i, part := range parts {
+		if part == "version" && i+1 < len(parts) {
+			return strings.TrimPrefix(strings.TrimSpace(parts[i+1]), "v")
 		}
 	}
+
 	return ""
 }
 
@@ -77,42 +67,55 @@ func RequestVersion(service *config.Service) {
 		return
 	}
 
-	var currentVersion *semver.Version
-	if service.Version.Current != "n/a" {
-		var err error
-		currentVersion, err = semver.NewVersion(service.Version.Current)
-		if err != nil {
-			logger.LogError(fmt.Sprintf("Current version format is invalid for %s: %v", service.BinName, err))
-			// Continue processing without a valid current version
-		}
-	}
-
 	for _, tag := range tags {
-		versionStr := strings.TrimPrefix(tag.Name, "v")
-		semVer, err := semver.NewVersion(versionStr)
+		tagVersionStr := strings.TrimPrefix(tag.Name, "v")
+		tagVersion, err := semver.NewVersion(tagVersionStr)
 		if err != nil {
-			logger.LogError(fmt.Sprintf("Invalid version format %s for %s: %v", versionStr, service.BinName, err))
+			logger.LogWarn(fmt.Sprintf("Tag contained invalid version format %s for %s: %v", tagVersionStr, service.BinName, err))
 			continue
 		}
 
-		// Update only if no valid current version or if the found version is greater
+		logger.LogDebug(fmt.Sprintf("Checking remote version tag %s for %s", tagVersionStr, service.BinName))
+
+		isDevVersion := false
 		if strings.Contains(tag.Name, "-dev") || strings.Contains(tag.Name, "-rc") {
-			if service.Version.Dev == "n/a" || (currentVersion != nil && semVer.GreaterThan(currentVersion)) {
-				service.Version.Dev = versionStr
-				config.UpdateService(service.BinName, service)
+			isDevVersion = true
+		}
+
+		currentVersionStr := "n/a"
+		if isDevVersion {
+			currentVersionStr = service.Version.Dev
+		} else {
+			currentVersionStr = service.Version.Latest
+		}
+
+		currentVersion, err := semver.NewVersion(currentVersionStr)
+		if err != nil {
+			if isDevVersion {
+				service.Version.Dev = tagVersionStr
+			} else {
+				service.Version.Latest = tagVersionStr
 			}
 		} else {
-			if service.Version.Latest == "n/a" || (currentVersion != nil && semVer.GreaterThan(currentVersion)) {
-				service.Version.Latest = versionStr
-				config.UpdateService(service.BinName, service)
+			if tagVersion.GreaterThan(currentVersion) {
+				if isDevVersion {
+					service.Version.Dev = tagVersionStr
+				} else {
+					service.Version.Latest = tagVersionStr
+				}
 			}
 		}
+		config.UpdateService(service.BinName, service)
 	}
 }
 
-func isSemVer(version string) bool {
-	_, err := semver.NewVersion(version)
-	return err == nil
+func isSemVer(version string) *semver.Version {
+	newVersion, err := semver.NewVersion(version)
+	if err != nil {
+		return nil
+	}
+
+	return newVersion
 }
 
 func IsVersionNewer(version1, version2 string) bool {
@@ -122,7 +125,8 @@ func IsVersionNewer(version1, version2 string) bool {
 
 	if version1 == "n/a" && version2 != "n/a" {
 		logger.LogDebug("Version 1 is n/a, checking if version 2 is a valid SemVer")
-		return isSemVer(version2)
+
+		return isSemVer(version2) != nil
 	}
 
 	if version1 != "n/a" && version2 == "n/a" {
@@ -135,5 +139,5 @@ func IsVersionNewer(version1, version2 string) bool {
 		logger.LogError(fmt.Sprintln("Error parsing versions:", err1, err2))
 		return false
 	}
-	return v1.GreaterThan(v2)
+	return v2.GreaterThan(v1)
 }
